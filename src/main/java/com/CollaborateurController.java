29bc13db52c0608b9dac7c8c4943e6b7e3ab4ba5
landscht <tony.landschoot@outlook.com>
2020-04-04 01:34:47 +0200

    fix cors

--- src/main/java/com/CollaborateurController.java
+++ src/main/java/com/CollaborateurController.java
@@ -15,45 +15,45 @@ public class CollaborateurController {
     @Autowired
     private CollaborateurRepository collaborateurRepository;
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = {"http://localhost:8080","https://projet-catho-front.herokuapp.com"})
     @PostMapping("/add")
     public @ResponseBody HttpStatus addNewColl(@RequestBody Collaborateur collaborateur) {
         collaborateurRepository.save(collaborateur);
         return HttpStatus.CREATED;
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = {"http://localhost:8080","https://projet-catho-front.herokuapp.com"})
     @GetMapping("/getAll")
     public @ResponseBody Iterable<Collaborateur> getAll() {
         return collaborateurRepository.findAll();
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = {"http://localhost:8080","https://projet-catho-front.herokuapp.com"})
     @GetMapping("/getAllByMatricule")
     public @ResponseBody Iterable<Collaborateur> getAllByMatricule(@RequestParam String matricule) {
         return collaborateurRepository.getAllByMatricule(matricule);
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = {"http://localhost:8080","https://projet-catho-front.herokuapp.com"})
     @GetMapping("/getAllMatricule")
     public @ResponseBody Iterable<String> getAllMatricule() {
         return collaborateurRepository.getAllMatricule();
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = {"http://localhost:8080","https://projet-catho-front.herokuapp.com"})
     @DeleteMapping("/deleteFicheById")
     public @ResponseBody HttpStatus deleteFicheById(@RequestParam long id) {
         collaborateurRepository.deleteById(id);
         return HttpStatus.OK;
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = {"http://localhost:8080","https://projet-catho-front.herokuapp.com"})
     @PutMapping("/updateById")
     public @ResponseBody Collaborateur updateById(@RequestBody Collaborateur collaborateur) {
         return collaborateurRepository.save(collaborateur);
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = {"http://localhost:8080","https://projet-catho-front.herokuapp.com"})
     @GetMapping("/getById")
     public ResponseEntity getById(@RequestParam long id) {
         Optional<Collaborateur> optional = collaborateurRepository.findById(id);
@@ -64,7 +64,7 @@ public class CollaborateurController {
         }
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = {"http://localhost:8080","https://projet-catho-front.herokuapp.com"})
     @GetMapping("/getAllByYear")
     public ResponseEntity getAllByYear(@RequestParam int year) {
         Iterable<Collaborateur> collaborateurs = collaborateurRepository.getAllByYear(year);
