5e58f8ab655e6adba3ca8e661a357c5e6d080a1e
landscht <tony.landschoot@outlook.com>
2020-04-05 01:42:11 +0200

    fix

--- src/main/java/com/security/WebSecurityConfig.java
+++ src/main/java/com/security/WebSecurityConfig.java
@@ -57,6 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/coll/getAll").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
